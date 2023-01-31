ad9484e38a128a4e4f73701d35fadb56661d9ca3
Sofya Kyzylova <121377923+SofyaKyzylova@users.noreply.github.com>
2023-01-31 23:42:26 +0500

    Update Main.kt
    major code improvements

--- AttestationTask/src/main/kotlin/Main.kt
+++ AttestationTask/src/main/kotlin/Main.kt
@@ -1,29 +1,97 @@
 import java.lang.NumberFormatException
 
-interface IVehicle {
-    fun countVehicles(){
-
-    }
-    fun distance()  {
-
-    }
-    fun absDistance()  {
-
-    }
-    fun gasStationVehiclesCount(){
-
-    }
-    fun vehiclesByActivityCount() {
-
-    }
-}
-
 interface TrafficLights {
-    fun greenLight() {
-        println("Зеленый свет светофора")
+
+    fun distance(drivingTime: Double, drivingSpeed: Int): Double {
+        return (drivingSpeed / 3.6) * drivingTime
     }
-    fun redLight(){
-        println("Красный свет светофора")
+
+    private fun startTrafficLights(carsArray: List<Car>, statisticsTime: Double): Int {
+        var carsPassed = 0
+        var carsSumDistance = 0.0
+        var carsSpeed = 0.0
+        var carAverageSpeed = 0.0
+        var carAverageDistance = 0.0
+
+        for (i in (carsArray.indices).random()..(carsArray.indices).random()) {
+            carsArray[i].speed = (0..60).random()
+            println(
+                "${carsArray[i].type} движется со скоростью ${carsArray[i].speed} км/с, " +
+                        "проехал ${distance(25.0, carsArray[i].speed)} метров"
+            )
+
+            carsSpeed += carsArray[i].speed
+            carsSumDistance += distance(25.0, carsArray[i].speed)
+
+            if (distance(25.0, carsArray[i].speed) >= 100) {
+                carsPassed += 1
+            }
+        }
+
+        carAverageSpeed = carsSpeed / carsArray.size
+        carAverageDistance = carsSumDistance / carAverageSpeed
+
+        val str = "\n Статистика светофора: \n" +
+                "За $statisticsTime минут светофор проехало $carsPassed автомобилей \n" +
+                "За $statisticsTime минут все машины проехали в сумме $carsSumDistance метров \n" +
+                "Средняя скорость машины на светофоре составляет $carAverageSpeed м/с \n" +
+                "За $statisticsTime минут автомобиль проезжает в среднем $carAverageDistance метров \n"
+        return carsPassed
+    }
+
+    fun traffic(carsArray: List<Car>)
+    {
+        println("Вы на светофоре!")
+        println("Один цикл состоит из зеленого (25 c) и красного света (55 с)")
+        println("Автомобиль преодолел светофор, если он проехал >= 100 метров за время зеленго сигнала \n")
+
+        print("Введите время сбора статистики в минутах: ")
+        var inputTime = readln()!!
+        var statisticsTime: Double = try {
+            inputTime.toDouble()
+        }
+        catch (e: NumberFormatException){
+            0.0
+        }
+        while(statisticsTime <= 0){
+            println("Вы ввели неверное время! Поробуйте еще раз:")
+            inputTime = readln()!!
+            statisticsTime = try {
+                inputTime.toDouble()
+            }
+            catch (e: NumberFormatException){
+                0.0
+            }
+        }
+
+        var statisticCycles = statisticsTime * 60
+        var allCarsPassed = 0
+        //var statistics: String = ""
+
+        println("Запуск светофора на $statisticsTime минут:")
+        while (statisticCycles > 0)
+        {
+            println("\n Зеленый сигнал светофора")
+            allCarsPassed += startTrafficLights(carsArray, statisticsTime)
+            //statistics = startTrafficLights(carsArray, statisticsTime)
+
+            try {
+                println("\n Красный сигнал светофора")
+                Thread.sleep(5500)
+            } catch (e: InterruptedException) {
+                e.printStackTrace()
+            }
+            statisticCycles -= 100
+        }
+
+        //println(statistics)
+
+        /**/
+        println("\n Статистика светофора:")
+        println("За $statisticsTime минут светофор проехало $allCarsPassed автомобилей")
+        println("За $statisticsTime минут все машины проехали в сумме $ метров")
+        println("Средняя скорость машины на светофоре составляет $ м/с")
+        println("За $statisticsTime минут автомобиль проезжает в среднем $ метров")
     }
 }
 
@@ -110,7 +178,7 @@ interface GasStation {
         var gasCost: Double
         if(gasNumber == 92)
             gasCost = 37.5
-        else gasCost = 40.2
+        else gasCost = 40.5
         var paymentCost = gasCost*gasLitres
 
         println("Ваши данные")
@@ -124,6 +192,103 @@ interface GasStation {
     }
 
     fun leaveGasStation()
+}
+
+interface RoadCafe {
+    fun goToRoadCafe()
+
+    private fun payment(paymentMethod: Int, paymentCost: Double){
+        if(paymentMethod == 1){
+            println("Введите сумму денег:")
+            var moneyInput = readln()!!
+            var money: Double = try {
+                moneyInput.toDouble()
+            }
+            catch (e: NumberFormatException){
+                0.0
+            }
+            while(money < paymentCost) {
+                println("Введеной суммы недостаточно!")
+                println("Введите сумму денег:")
+                money = readln().toDouble()
+            }
+            if(money >= paymentCost){
+                println("Ваша сдача: ${money - paymentCost} \n")
+            }
+        }
+        else {
+            println("Оплата прошла успешно! \n")
+        }
+    }
+
+    fun buyFood() {
+        println("Выберите номер товара")
+        println("1 - Булочка")
+        println("2 - Пицца")
+        println("3 - Почик")
+
+        var inputItemNumber = readln()!!
+        var itemNumber: Int
+        itemNumber = try {
+            inputItemNumber.toInt()
+        } catch (e: NumberFormatException) {
+            4
+        }
+        while (itemNumber <= 0 || itemNumber > 3){
+            println("Введен неверный номер товара! Пожалуйста, повторите ввод:")
+            inputItemNumber = readln()!!
+            itemNumber = try {
+                inputItemNumber.toInt()
+            } catch (e: NumberFormatException) {
+                4
+            }
+        }
+
+        println("Введите количество:")
+        var itemAmountInput = readln()!!
+        var itemAmount: Double = try{
+            itemAmountInput.toDouble()
+        } catch (e: NumberFormatException){
+            .0
+        }
+        while(itemAmount <= 0){
+            println("Введено неверное количество! Пожалуйста, повторите ввод:")
+            itemAmountInput = readln()!!
+            itemAmount = try{
+                itemAmountInput.toDouble()
+            } catch (e: NumberFormatException){
+                .0
+            }
+        }
+
+        var itemCost: Double
+        var itemName: String
+        when (itemNumber) {
+            1 -> {
+                itemCost = 30.0
+                itemName = "Булочка"
+            }
+            2 -> {
+                itemCost = 85.0
+                itemName = "Пицца"
+            }
+            else -> {
+                itemCost = 60.0
+                itemName = "Пончик"
+            }
+        }
+        var paymentCost = itemCost*itemAmount
+
+        println("Ваши данные")
+        println("Товар: $itemName \n" +
+                "Количество: $itemAmount \n" +
+                "ИТОГО к оплате: $paymentCost")
+        println("Выберите способ оплаты: 1 - наличные, 2 - банковская карта")
+        var paymentMethod = readln().toInt()
+        payment(paymentMethod, paymentCost)
+    }
+
+    fun leaveRoadCafe()
 }
 
 open abstract class Vehicle(open var type: String)
@@ -160,7 +325,7 @@ open abstract class Vehicle(open var type: String)
 }
 
 
-open class Car(type: String = "Автомобиль"): Vehicle(type), GasStation, TrafficLights, IVehicle
+open class Car(type: String = "Автомобиль"): Vehicle(type), GasStation, TrafficLights, RoadCafe, IVehicle
 {
     override var tires = "Автомобильные шины"
     override var roadSurface = "Любое дорожное покрытие"
@@ -207,6 +372,14 @@ open class Car(type: String = "Автомобиль"): Vehicle(type), GasStation
 
     override fun leaveGasStation() {
         println("${this.type} с номером ${this.serialNumber} покинул автозаправку \n")
+    }
+
+    override fun goToRoadCafe() {
+        println("${this.type} с номером ${this.serialNumber} остановился в дорожном кафе  \n")
+    }
+
+    override fun leaveRoadCafe() {
+        println("${this.type} с номером ${this.serialNumber} покинул дорожное кафе  \n")
     }
 }
 
@@ -296,7 +469,7 @@ open class Trolleybus(type: String = "Троллейбус"): Bus(type) //DOES N
 }
 
 
-open abstract class Train(type: String = "Поезд"): Vehicle(type), TrafficLights, IVehicle
+open abstract class Train(type: String = "Поезд"): Vehicle(type), IVehicle
 {
     override var tires = "Колесная пара"
     override var roadSurface = "Железнодорожные рельсы"
@@ -343,7 +516,7 @@ open class Tram(type: String = "Трамвай"): Train(type)
 }
 
 
-open class Bike(type: String = "Велосипед"): Vehicle(type), TrafficLights, IVehicle
+open class Bike(type: String = "Велосипед"): Vehicle(type), RoadCafe, IVehicle
 {
     override var tires = "Велосипедные шины"
     override var roadSurface = "Асфальт"
@@ -357,6 +530,14 @@ open class Bike(type: String = "Велосипед"): Vehicle(type), TrafficLigh
         this.speed = speed
         this.serialNumber = serialNumber
         println("An instance of the Bike class was successfully created.")
+    }
+
+    override fun goToRoadCafe() {
+        println("${this.type} с номером ${this.serialNumber} остановился в дорожном кафе  \n")
+    }
+
+    override fun leaveRoadCafe() {
+        println("${this.type} с номером ${this.serialNumber} покинул дорожное кафе  \n")
     }
 }
 
@@ -430,7 +611,6 @@ class Station<T>() //station for buses or trains
     private var isOnStation: Boolean = false
     private var vehiclesCount: Int = 0
 
-    //this - обращается к busStation!!!!!
     constructor(t: T) : this() {
         println("$t был добавлен на станцию \n")
         this.isOnStation = true
@@ -478,7 +658,9 @@ fun main() {
 
     val car1 = Car(110,111, "красный", "Mercedes-Benz", "AMG GT")
     val car2 = Car(90, 222, "белый", "Nissan", "Quashqai")
-    val car3 = Car(110,333, "зеленый", "Kia", "Rio")
+    val car3 = Car(105,333, "зеленый", "Honda", "Civic")
+    val car4 = Car(70,458, "желтый", "Kia", "Rio")
+    val car5 = Car(60,267, "черный", "Lada", "Granta")
 
     val truck1 = Truck(70, 1547, "оранжевый", "SHACMAN", "6Х4 X3000", 3.2)
     val truck2 = Truck("Автомобиль-манипулятор", 55, 4587, "белый", "Nissan", "Diesel UD", 10.0)
@@ -498,12 +680,12 @@ fun main() {
     val scooter1 = Motorcycle("Скутер", "Шины для скутера", "Теплое время года", 70, 1110, "красный","Racer", "RC150T-15 STELLS SPORT")
     val moped1 = Motorcycle("Мопед", "Шины для скутера", "Теплое время года",60, 1303, "желтый", "Racer", "RC50QT-6F Flame")
 
-    val vehicles = setOf<Vehicle>(car1, car2, car3, truck1, truck2, bus1, bus2, bus3, trolleybus1, tram1,
+    val vehicles = setOf<Vehicle>(car1, car2, car3, car4, car5, truck1, truck2, bus1, bus2, bus3, trolleybus1, tram1,
         train1, train2, bike1, motorcycle1, motorcycle2, scooter1, moped1)
-    for(vehicle in vehicles)
-    {
-        vehicle.startMovement()
-    }
+    for(vehicle in vehicles) { vehicle.info() }
+    println()
+
+    for(vehicle in vehicles) { vehicle.startMovement() }
     println()
 
     /*
@@ -520,13 +702,86 @@ fun main() {
     busStation.info()*/
 
 
-    //println("Выберите вариант дорожно-транспортной ситуации:")
 
+    println("Выберите вариант дорожно-транспортной ситуации:")
+    println("1 - Светофор")
+    println("2 - Автозаправка")
+    println("3 - Дорожное кафе")
 
-    car1.goToGasStation()
-    car1.refuel()
-    car1.leaveGasStation()
+    var input = readln()!!
+    var option: Int
+    option = try {
+        input.toInt()
+    } catch (e: NumberFormatException) {
+        4
+    }
+    while (option <= 0 || option > 3){
+        println("Введен неверный номер! Пожалуйста, повторите ввод:")
+        input = readln()!!
+        option = try {
+            input.toInt()
+        } catch (e: NumberFormatException) {
+            4
+        }
+    }
 
-    truck2.goToGasStation()
-    motorcycle2.goToGasStation()
+    when (option) {
+        1 -> {
+            val carsArray = arrayListOf<Car>(car1, car2, car3, car4, car5, truck1, truck2, bus1, bus2, bus3)
+            car1.traffic(carsArray)
+            println()
+        }
+        2 -> {
+            car1.goToGasStation()
+            car1.refuel()
+            println("Хотите покинуть автозаправку? (1/0)")
+            var inputGasStation = readln()!!
+            var optionGasStation: Int
+            optionGasStation = try {
+                inputGasStation.toInt()
+            } catch (e: NumberFormatException) {
+                2
+            }
+            while (optionGasStation != 0 && optionGasStation != 1){
+                println("Введен неверный номер! Пожалуйста, повторите ввод:")
+                inputGasStation = readln()!!
+                optionGasStation = try {
+                    inputGasStation.toInt()
+                } catch (e: NumberFormatException) {
+                    2
+                }
+            }
+            if(optionGasStation == 1)
+                car1.leaveGasStation()
+            println()
+        }
+        3 -> {
+            motorcycle2.goToRoadCafe()
+            motorcycle2.buyFood()
+            println("Хотите покинуть дорожное кафе? (1/0)")
+            var inputRoadCafe = readln()!!
+            var optionRoadCafe: Int
+            optionRoadCafe = try {
+                inputRoadCafe.toInt()
+            } catch (e: NumberFormatException) {
+                2
+            }
+            while (optionRoadCafe != 0 && optionRoadCafe != 1){
+                println("Введен неверный номер! Пожалуйста, повторите ввод:")
+                inputRoadCafe = readln()!!
+                optionRoadCafe = try {
+                    inputRoadCafe.toInt()
+                } catch (e: NumberFormatException) {
+                    2
+                }
+            }
+            if(optionRoadCafe == 1)
+                car1.leaveGasStation()
+            println()
+        }
+    }
+
+    for(vehicle in vehicles) { vehicle.stopMovement() }
+    println()
+
 }
